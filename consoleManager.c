d1b215fc7fc6155657252487725f0dd7decaed4f
Victor MASIAK <victor.masiak@telecom-paris.fr>
2021-05-19 16:22:00 +0200

    Centered ASCII art

--- consoleManager.c
+++ consoleManager.c
@@ -19,19 +19,18 @@
 
 enum Color{BLACK, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, WHITE, NO_COLOR};
 
-const char* APATE[] = {"  /$$$$$$                       /$$              \n",
-                       " /$$__  $$                     | $$              \n",
-                       "| $$  \\ $$  /$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$ \n",
-                       "| $$$$$$$$ /$$__  $$|____  $$|_  $$_/   /$$__  $$\n",
-                       "| $$__  $$| $$  \\ $$ /$$$$$$$  | $$    | $$$$$$$$\n",
-                       "| $$  | $$| $$  | $$/$$__  $$  | $$ /$$| $$_____/\n",
-                       "| $$  | $$| $$$$$$$/  $$$$$$$  |  $$$$/|  $$$$$$$\n",
-                       "|__/  |__/| $$____/ \\_______/   \\___/   \\_______/\n",
-                       "          | $$                                   \n",
-                       "          | $$                                   \n",
+const char* APATE[] = {"  /$$$$$$                       /$$              ",
+                       " /$$__  $$                     | $$              ",
+                       "| $$  \\ $$  /$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$ ",
+                       "| $$$$$$$$ /$$__  $$|____  $$|_  $$_/   /$$__  $$",
+                       "| $$__  $$| $$  \\ $$ /$$$$$$$  | $$    | $$$$$$$$",
+                       "| $$  | $$| $$  | $$/$$__  $$  | $$ /$$| $$_____/",
+                       "| $$  | $$| $$$$$$$/  $$$$$$$  |  $$$$/|  $$$$$$$",
+                       "|__/  |__/| $$____/ \\_______/   \\___/   \\_______/",
+                       "          | $$                                   ",
+                       "          | $$                                   ",
                        "          |__/                                   "};
 
-//Reference: https://stackoverflow.com/questions/1022957/getting-terminal-width-in-c
 int getTerminalWidth(){
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -42,12 +41,27 @@ void setConsoleColor(int colorID){
     printf("\033[0;3%dm", colorID);
 }
 
+void surroundText(char* text, char placeholder){
+    int terminalWidth = getTerminalWidth();
+    int textWidth = (int) strlen(text) + 2;
+    int leftWidth = (terminalWidth - textWidth) / 2;
+    int rightWidth = terminalWidth - (leftWidth + textWidth);
+
+    for (int i=0 ; i<leftWidth; i++){
+        printf("%c", placeholder);
+    }
+    printf(" %s ", text);
+    for (int i=0 ; i<rightWidth ; i++){
+        printf("%c", placeholder);
+    }
+    printf("\n");
+}
+
 void printTitle(){
     setConsoleColor(BLUE);
-    printf("\n");
-    const int k = 11;
-    for (int i=0 ; i<k ; i++){
-        printf("%s", APATE[i]);
+    printf("\n\n");
+    for (int i=0 ; i<11 ; i++){
+        surroundText(APATE[i], ' ');
     }
 }
 
@@ -55,24 +69,12 @@ void printHeader(char* sectionName){
 
     setConsoleColor(YELLOW);
 
-    int terminalWidth = getTerminalWidth();
-    int sectionWidth = (int) strlen(sectionName) + 2;
-    int leftWidth = (terminalWidth-sectionWidth)/2;
-    int rightWidth = terminalWidth - (leftWidth+sectionWidth);
-
-    printf("\n\n\n");
-
-    for (int i=0 ; i<leftWidth; i++){
-        printf("=");
-    }
-    printf(" %s ", sectionName);
-    for (int i=0 ; i<rightWidth ; i++){
-        printf("=");
-    }
     printf("\n\n");
+    surroundText(sectionName, '=');
+    printf("\n");
 
     setConsoleColor(NO_COLOR);
-    for (int i=0 ; i<terminalWidth; i++){
+    for (int i=0 ; i<getTerminalWidth(); i++){
         printf("-");
     }
     printf("\n");
