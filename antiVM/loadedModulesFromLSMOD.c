8dbc916765033d8a9b20ea7226b85da9712c192b
Victor MASIAK <victor.masiak@telecom-paris.fr>
2021-05-20 13:40:38 +0200

    Correction for the lsmod check

--- antiVM/loadedModulesFromLSMOD.c
+++ antiVM/loadedModulesFromLSMOD.c
@@ -7,17 +7,18 @@
 
 //https://www.ekkosec.com/blog/2018/3/15/linux-anti-vm-how-does-linux-malware-detect-running-in-a-virtual-machine-
 //TODO: move list in a config file
-const char* KNOWN_VM_MODULES[]   = {"Vboxsf", "vboxguest",                                            //VirtualBox
-                                    "vmw_ballon", "vmxnet", "vmw",                                    //VMware
-                                    "xen-vbd", "xen-vnif",                                            //Xen
-                                    "virtio_pci", "virtio_net",                                       //Qemu\KVM
-                                    "hv_vmbus", "hv_blkvsc", "hv_netvsc", "hv_utils", "hv_storvsc"};  //MS Hyper-V
+const char* KNOWN_VM_MODULES[]   = {"Vboxsf ", "vboxguest ",                                               //VirtualBox
+                                    "vmw_ballon ", "vmxnet ", "vmw ",                                      //VMware
+                                    "xen-vbd ", "xen-vnif ",                                               //Xen
+                                    "virtio_pci ", "virtio_net ",                                          //Qemu\KVM
+                                    "hv_vmbus ", "hv_blkvsc ", "hv_netvsc ", "hv_utils ", "hv_storvsc ",   //MS Hyper-V
+                                    };
 const int KNOWN_VM_MODULES_SIZE  = 14;
 
 int checkLoadedModulesFromLSMOD(){
 
     FILE* file;
-    file = fopen("/sbin/lsmod", "rb");
+    file = fopen("/proc/modules", "rb");
     int result, tmp;
 
     if (file == NULL) {
--- aux.c
+++ aux.c
@@ -3,7 +3,7 @@
 int checkWordInFile(FILE* file, char* str){
     const int tempSize = 1024;
     char temp[tempSize];
-    fseek(file, 0, SEEK_SET);
+    rewind(file);
     while(fgets(temp, tempSize, file) != NULL) {
         if((strstr(temp, str)) != NULL) {
             return RESULT_FAILURE;
