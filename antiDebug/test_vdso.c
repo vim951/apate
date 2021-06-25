08f55eed4c27c2b263b47cd04ed1209c24b8412e
Victor MASIAK <victor.masiak@telecom-paris.fr>
2021-06-26 00:09:12 +0200

    added verbose description

--- antiDebug/test_vdso.c
+++ antiDebug/test_vdso.c
@@ -11,21 +11,26 @@ int test_vdso_detect(char* resultDescriptionBuffer)
 
     if (this_arch == ARCH_ARM64 || this_arch == ARCH_I386) {
         /* This trick unfortunately does not work on 32bit x86 and ARM64 */
+        strcat(resultDescriptionBuffer, "--> This trick unfortunately does not work on 32bit x86 and ARM64.\n");
         return RESULT_UNKNOWN;
     }
 
     if (!vdso) {
         /* Auxiliary vector does not contain vdso entry. Unknown result. */
+        strcat(resultDescriptionBuffer, "--> Auxiliary vector does not contain vdso entry. Unknown result.\n");
         return RESULT_UNKNOWN;
     }
 
     if (!(aslr_active() == RESULT_FAILURE)) {
         /* No ASLR on this machine. Unknown result */
+        strcat(resultDescriptionBuffer, "--> No ASLR on this machine. Unknown result.\n");
         return RESULT_UNKNOWN;
     }
 
-    if ((unsigned long)&tos > vdso)
+    if ((unsigned long)&tos > vdso) {
+        strcat(resultDescriptionBuffer, "--> &tos > vdso\n");
         return RESULT_FAILURE;
+    }
     else
         return RESULT_SUCCESS;
 }
