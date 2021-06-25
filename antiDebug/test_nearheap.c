f82e2db6ac7b7c1df69f533ae33999811e1200ec
Victor MASIAK <victor.masiak@telecom-paris.fr>
2021-06-25 23:46:57 +0200

    added verbose description

--- antiDebug/test_nearheap.c
+++ antiDebug/test_nearheap.c
@@ -11,6 +11,9 @@ int test_nearheap_detect(char* resultDescriptionBuffer)
     unsigned char *probe = malloc(0x10);
 
     if (probe - &bss > 0x20000) {
+        char txt[100];
+        snprintf(txt, 99, "--> The distance between the two probes (in the heap and in the bss section) is only %ld.\n", probe - &bss);
+        strcat(resultDescriptionBuffer, txt);
         return RESULT_SUCCESS;
     } else {
         return RESULT_FAILURE;
